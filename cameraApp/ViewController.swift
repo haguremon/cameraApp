fe479a778d3ea7c90cfc28dcfdb7424c59249be1
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-29 10:39:31 +0900

    Initial Commit

---
+++ cameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cameraApp
+//
+//  Created by IwasakIYuta on 2021/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
